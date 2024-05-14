b538019da5f4d4bac6d12e03bbbcbfadc5842c51
Yrysbeav <mzamirbekovic02@gmail.com>
2024-05-13 23:16:14 -0500

    Learning the pushing the branches

---
+++ all_checks.py
@@ -0,0 +1,20 @@
+
+def check_root_full():
+    """Returns True if the root partition is full, False otherwise."""
+    return check_disk_full(disk="/", min_gb=2, min_percent=10)
+
+
+def main():
+    checks = [
+            (check_reboot, "Pending Reboot."),
+            (check_root_full, "Root partition full"),
+            ]
+
+    for check, msg in checks:
+        if check():
+            print(msg)
+            sys.exit(1)
+
+    print("Everything ok.")
+    sys.exit(0)
+    
